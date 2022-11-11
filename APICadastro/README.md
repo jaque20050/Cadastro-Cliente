ff935bfdf7c58475d5301a085a91d08d0f043cd0
jaque20050 <jaque20050@hotmail.com>
2022-11-11 15:52:15 -0300

    Front-end inicial, Back-end junto com o MySql

---
+++ APICadastro/README.md
@@ -0,0 +1,6 @@
+# Cadastro-Cliente
+Projeto Web API para validação de Cadastro do cliente
+
+Diagrama Funcional do Cadastro
+
+![image](https://user-images.githubusercontent.com/55770645/201347543-3c689f3a-1418-4f48-a05a-4b47b509b879.png)
---
+++ APICadastro/script.js
@@ -0,0 +1,49 @@
+const formulario = document.querySelector("form");
+
+const Inome = document.querySelector(".nome");
+const Iemail = document.querySelector(".email");
+const Isenha = document.querySelector(".senha");
+const Itelefone = document.querySelector(".telefone");
+
+function cadastrar() {
+
+    //Será responsavel por acessar a API
+    fetch("http://localhost:8080/cadastrar",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            //Formato JSON do Objeto Cadastrar
+            method: "POST",
+            body: JSON.stringify({
+                nome: Inome.value,
+                email: Iemail.value,
+                senha: Isenha.value,
+                telefone: Itelefone.value
+
+                //console.log(dados)
+
+            })
+        })
+        .then(function (res) { console.log(res) })
+        .catch(function (res) { console.log(res) })
+
+};
+
+function limparCache() {
+    Inome.value = "";
+    Iemail.value = "";
+    Isenha.value = "";
+    Itelefone.value = "";
+};
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    //Chamando a função 
+    cadastrar();
+    limparCache();
+
+});

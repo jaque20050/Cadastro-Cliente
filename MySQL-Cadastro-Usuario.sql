94f1b869473410b7e79926403d47a2e1892cf064
jaque20050 <jaque20050@hotmail.com>
2022-11-18 17:15:29 -0300

    Endpoints

---
+++ MySQL-Cadastro-Usuario.sql
@@ -0,0 +1,19 @@
+create database banco_de_usuarios;
+
+use banco_de_usuarios;
+
+create table usuario(
+ id integer auto_increment primary key,
+ nome_completo varchar(200) not null,
+ email varchar(50) not null,
+ senha text not null,
+ telefone varchar(15) not null
+);
+
+#ALTER TABLE usuario MODIFY COLUMN email varchar(50) not null;
+
+insert into usuario values (null, 'eni','eni@hotmail.com', '123456', '119330204565');
+
+select * from usuario;
+
+SELECT * FROM banco_de_usuarios.usuario;
--- projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -7,13 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Entity
 @Table(name = "usuario")
-@Getter
-@Setter
 public class Usuario {
 
 	@Id
@@ -32,4 +27,45 @@ public class Usuario {
 
 	@Column(name = "telefone", length = 15, nullable = true)
 	private String telefone;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome_completo() {
+		return nome_completo;
+	}
+
+	public void setNome_completo(String nome_completo) {
+		this.nome_completo = nome_completo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 }

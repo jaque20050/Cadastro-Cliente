9f96fc76ccae6b3280bd4ea4d1e57af8ad7d0e07
jaque20050 <jaque20050@hotmail.com>
2022-11-25 16:32:32 -0300

    Lombok

--- projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -7,6 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "usuario")
 public class Usuario {
@@ -27,45 +32,5 @@ public class Usuario {
 
 	@Column(name = "telefone", length = 15, nullable = true)
 	private String telefone;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
 
 }
